fa03198b2dc6d540adb48636187e423eeed091dd
Camilo Bayona <drcamilob@live.com>
2019-09-19 22:24:00 -0500

    Answering the question 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char num = '0';
+
+	for (num = '0' ; num <= '9' ; num++)
+		putchar(num);
+return (0);
+}
