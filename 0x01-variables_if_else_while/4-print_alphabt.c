92a5465662ab4a0ddec0a2a9c50b462aff6e7184
cbodev <drcamilob@live.com>
2019-09-20 00:01:48 +0000

    Answering the question 4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+		if (alpha != 'q' && alpha != 'e')
+			putchar(alpha);
+return (0);
+}
