0058875cc176029cabe894d917914c73d3fdaa6c
cbodev <drcamilob@live.com>
2019-09-26 03:29:47 +0000

    Answering the question 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - Check the function
+ * @a: Integer
+ * @b: Integer
+ * Return: Result
+ */
+int add(int a, int b)
+{
+	int suma = a + b;
+
+	return (suma);
+	_putchar ('\n');
+}
