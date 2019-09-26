4e72354a398ca0ae0d6eba501be48fa6e8c83557
cbayonao <drcamilob@live.com>
2019-09-26 22:20:53 +0000

    Answering the question 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_line - Check the function
+ * @n: integer.
+ * Return: no return
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int x;
+		for (x = 1; x <= n; x++)
+		_putchar ('_');
+	}
+_putchar ('\n');
+}
