6cd88b7a1360a3e3c29e0c30fc29b0e49466b1bb
cbayonao <drcamilob@live.com>
2019-09-26 19:26:00 +0000

    Answering the question 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * mul - Check the function
+ * @a: Integer
+ * @b: Integer
+ * Return: 0 Always.
+ */
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+	return (m);
+}
