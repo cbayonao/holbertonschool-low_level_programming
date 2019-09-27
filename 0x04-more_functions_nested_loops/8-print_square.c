7bf3969c3c3b6f03d1d5495d74a2bea1045945c0
cbayonao <drcamilob@live.com>
2019-09-27 00:50:35 +0000

    Answering the question 8-squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_square - Check the function
+ * @n: integer.
+ * Return: no return
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (y = 1; y <= size; y++)
+		{
+			for (x = 1; x <= size; x++)
+			{
+				_putchar(35);
+			}
+		_putchar('\n');
+		}
+	}
+	else if (size <= 0)
+	{
+		_putchar ('\n');
+	}
+}
