08a89b9b36e296243ece1b4c6a738e0f3c5a3021
CamiloBO <drcamilob@live.com>
2019-09-28 21:51:19 +0000

    Answering the question 3 Remaining days

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -6,3 +6,18 @@
 void positive_or_negative(int i);
 
 #endif /* HOLBERTON_H */
+
+#ifndef LARGEST_H
+#define LARGEST_H
+int largest_number(int a,int b,int c);
+#endif /*HOLBERTON*/
+
+#ifndef LEAP_H
+#define LEAP_H
+int convert_day(int month, int day);
+#endif
+
+#ifndef LEAP2_H
+#define LEAP2_H
+void print_remaining_days(int month, int day, int year);
+#endif
