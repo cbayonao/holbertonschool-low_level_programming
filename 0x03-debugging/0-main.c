00fc5c6dc460f3596d97bd9f0cd3e318a91dbbe5
CamiloBO <drcamilob@live.com>
2019-09-28 21:34:50 +0000

    Answering the question 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
 
 /**
-* main - tests the function if is positive or negative
+* main - tests the function.
 * Return: 0
 */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
-	return (0);
+
+	ireturn (0);
 }
