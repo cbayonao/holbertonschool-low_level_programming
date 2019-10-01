c8eb0318c42a9442ed9f01d8a6bd74bf3282bef2
cbayonao <drcamilob@live.com>
2019-10-01 20:27:14 +0000

    Answering the question 5 rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+* print_rev - put the contain of the variable in reverse
+* @s: character
+* Return: return the length of the string.
+*/
+void rev_string(char *s)
+{
+	int i;
+	int n = 0;
+	int swap = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		n++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		swap = s[i];
+		s[i] = s[n - 1];
+		s[n - 1] = swap;
+		n--;
+	}
+}
