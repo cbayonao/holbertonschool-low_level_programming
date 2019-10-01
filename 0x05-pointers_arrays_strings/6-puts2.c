c4f9b5ad1b4c436527070aaa38c3fe6d1e3144c9
cbayonao <drcamilob@live.com>
2019-10-01 22:27:03 +0000

    Answering the question 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+* puts2 - put the contain of the variable in reverse
+* @str: character
+* Return: No return.
+*/
+void puts2(char *str)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; str[x] != '\0'; x++)
+	
+	y = 0;
+	while (y != x)
+	{
+		if (z == 0)
+		{
+			_putchar (str[y]);
+		}
+		y = y + 1;
+		z = y % 2;
+	}
+_putchar('\n');
+}
