c80a609f9f9dd224b745df930784bcdf21d69506
cbayonao <drcamilob@live.com>
2019-10-02 17:08:38 +0000

    Answering the question 8-print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
-* print_array put the contain of the variable in reverse
-* @s: character
+* print_array - Short description
+* @a: integer
+* @n: integer
 * Return: No return.
 */
 void print_array(int *a, int n)
 {
 	int x;
-	
+
 	for (x = 0; x <= n - 1; x++)
 	{
 		if (x == n - 1)
