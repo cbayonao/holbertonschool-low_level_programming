e0c01a4096563db65506a73ead91f9004f0c539a
cbayonao <drcamilob@live.com>
2019-10-03 23:50:19 +0000

    1 try 5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* string_toupper - put the contain of the variable in reverse
+* @s: character
+* Return: No return.
+*/
+char *string_toupper(char *s)
+{
+	int i;
+
+	for(i = 0; s[i]!='\0'; i++)
+	{
+		if(s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i]=s[i]-32;
+		}
+	}
+return(s);
+}
