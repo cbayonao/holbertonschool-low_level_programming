4a1bf9bc2060394df8f21f60a4606790203db41d
cbayonao <drcamilob@live.com>
2019-10-03 19:44:07 +0000

    Answering the question 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* _strcmp - The name of the function.
+* @s1:  the contain of the variable in reverse
+* @s2: character
+* Return: x.
+*/
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
