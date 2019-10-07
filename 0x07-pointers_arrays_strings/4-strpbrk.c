fae3621907073b490d6741d5601e4e63dcd1e6a5
cbayonao <drcamilob@live.com>
2019-10-07 22:40:09 +0000

    Answer for 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,14 +2,13 @@
 /**
 * _strpbrk - The name of the function.
 * @s:  the contain of the variable in reverse
-* @b: character
-* @n: integer unsigned
+* @accept: character
 * Return: s.
 */
 char *_strpbrk(char *s, char *accept)
 {
 	while (*s)
-		if(_strchr(accept, *s++))
-			return ((char *)--s);
+		if (_strchr(accept, *s++))
+			return ((char *) --s);
 	return (0);
 }
