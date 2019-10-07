0d33e4370a14f50f8c9118ad89f1a24e4080e57d
cbayonao <drcamilob@live.com>
2019-10-07 22:55:48 +0000

    Answer for 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,8 +7,31 @@
 */
 char *_strpbrk(char *s, char *accept)
 {
-	while (*s)
-		if (_strchr(accept, *s++))
-			return ((char *) --s);
-	return (0);
+	int i, j, pos, flag = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	pos = i;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				if (j <= pos)
+				{
+					pos = j;
+					flag = 1;
+				}
+			}
+		}
+	}
+if (flag == 1)
+{
+	return (&s[pos]);
+}
+else
+{
+	return ('\0');
+}
 }
