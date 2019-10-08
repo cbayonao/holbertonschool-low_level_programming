62469ee77813ceee4dab574f212c9b7626e1ac1c
cbayonao <drcamilob@live.com>
2019-10-08 19:59:53 +0000

    Por fin

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+nclude <stdio.h>
+#include "holberton.h"
+/**
+* print_to_98 - check the code for Holberton School students.
+*@n: integer.
+* Return: Always 0.
+*/
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%d", n);
+		printf(", ");
+			if (n < 98)
+			{
+				n++;
+			}
+			else
+			{
+				n--;
+			}
+	}
+printf("98");
+putchar('\n');
+}
