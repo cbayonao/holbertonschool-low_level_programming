1374ddc2632d01213981419e5070749db3d2e99e
CamiloBO <drcamilob@live.com>
2019-10-08 03:24:07 +0000

    Answer for the 9-set_string.c

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* set_string - The name of the function.
+* @s:  the contain of the variable in reverse
+* @to: character
+* Return: no return.
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
