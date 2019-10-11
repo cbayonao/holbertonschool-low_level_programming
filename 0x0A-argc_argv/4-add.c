c1e353968fc54fd0b22e1a5b3087581bf7341775
cbayonao <drcamilob@live.com>
2019-10-11 23:45:50 +0000

    Answer for 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Function for mynameis.
+ *@argc: integer.
+ *@argv: character.
+ * Return: 0 Always.
+ */
+int main(int argc, char **argv)
+{
+	int i,
+	int res = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+	else
+	{
+			for (i = 1; i < argc; i++)
+			{
+				if (*argv[i] > 48 && *argv[i] < 57)
+				{
+					res = res + atoi(argv[i]);
+				}
+				else
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		printf("%d\n", res);
+	}
+	return (0);
+}
