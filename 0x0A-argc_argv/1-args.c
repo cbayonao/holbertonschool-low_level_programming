db2a783d506c5041fa5f2c9ef512e5edf553849d
cbayonao <drcamilob@live.com>
2019-10-11 19:40:43 +0000

    Answer for 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Function for mynameis.
+ *@argc: integer.
+ *@argv: character.
+ * Return: 0 Always.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv[0];
+	printf("%d\n", argc - 1);
+return (0);
+}
