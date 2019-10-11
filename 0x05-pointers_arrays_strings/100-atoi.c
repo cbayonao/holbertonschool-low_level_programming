8963d588a264e9bcc8e0a67376f24784f598b96b
cbayonao <drcamilob@live.com>
2019-10-11 16:54:01 +0000

    Answer for 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * _atoi - check the code for Holberton School students.
+ * @s: char
+ * Return: res.
+ */
+int _atoi(char *s)
+{
+	int i, res, sig;
+
+	i = 0;
+	res = 0;
+	sig = 1;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10 + (s[i] - '0');
+			if ((s[i + 1] < '0') || (s[i + 1] > '9'))
+			{
+				break;
+			}
+		}
+		if (s[i] == '-')
+		{
+			sig *= -1;
+		}
+		i++;
+	}
+	res = res * sig;
+return (res);
+}
