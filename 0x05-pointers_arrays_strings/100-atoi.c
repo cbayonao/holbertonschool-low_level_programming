81093887b48b4294aa663ff90ed9f319c90328b6
cbayonao <drcamilob@live.com>
2019-10-11 17:07:42 +0000

    fixing

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,29 +1,34 @@
 #include "holberton.h"
+#include <stdio.h>
+
 /**
- * _atoi - check the code for Holberton School students.
- * @s: char
- * Return: res.
+ * _atoi - Transform a char in int variable.
+ * @s: The char characters that be as input and it will change in int variable
+ * Return: The conversion.
  */
 int _atoi(char *s)
 {
-	int i, res, sig;
+	unsigned int res = 0;
+	int i, siesdig, noeschar, signo;
 
-	i = 0;
-	res = 0;
-	sig = 1;
-
-	while (s[i] != '\0')
+	siesdig = 0;
+	signo = 1;
+	for (i = 0; s[i] != '\0'; ++i)
 	{
+		if (s[i] == '-')
+			signo *= -1;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			res = res * 10 + (s[i] - '0');
-			if ((s[i + 1] < '0') || (s[i + 1] > '9'))
-				break;
+			res = res * 10 + s[i] - '0';
+			siesdig = 1;
+			noeschar = 1;
 		}
-		if (s[i] == '-')
-			sig *= -1;
-		i++;
+		else
+		{
+			noeschar = 0;
+		}
+		if (siesdig == 1 && noeschar == 0)
+			break;
 	}
-	res = res * sig;
-return (res);
+	return (res * signo);
 }
