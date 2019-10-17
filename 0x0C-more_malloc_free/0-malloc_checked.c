ec7c50eef5d6513b6e9848372d1c5c5b79a3b53f
cbayonao <drcamilob@live.com>
2019-10-17 17:57:25 +0000

    Answer for 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * malloc_checked - function that alocate memory using malloc.
+ * @b: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+	if (p == NULL)
+	{
+		exit (98);
+	}
+	return (p);
+}
