f6e38f6b67bdf428fdd9227f86ec553937315572
cbayonao <drcamilob@live.com>
2019-10-17 21:39:14 +0000

    Answer for 2-calloc3

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,11 +13,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *array;
 	unsigned int i;
 
-		array = malloc(nmemb * size);
 
 		if ((nmemb == 0) || (size == 0))
 			return (NULL);
-		else if ((array != '\0') && (size != 0))
+		array = malloc(nmemb * size);
+		if ((array != '\0') && (size != 0))
 		{
 			for (i = 0; i < (nmemb * size); i++)
 			{
