8a75104d208836f04824da2aec415db1f7e34a4e
cbayonao <drcamilob@live.com>
2019-10-19 19:10:20 +0000

    Recueperando pointers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 /**
  * print_to_98 - check the code.
- *@n: The variable taht take the valio of the main and put in this program.
+ * @n: The variable taht take the valio of the main and put in this program.
  * Return: Always 0.
  */
 void print_to_98(int n)
 {
-	while (n != 98)
+	for (; n != 98; )
 	{
 		printf("%d, ", n);
 		if (n > 98)
