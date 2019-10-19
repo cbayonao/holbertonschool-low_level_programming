a8edac0fe0ba9c9c2bc99e48832dbf206b95c329
cbayonao <drcamilob@live.com>
2019-10-19 18:43:17 +0000

    Recuperando puntos perdiods

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,39 +8,22 @@
  */
 void print_to_98(int n)
 {
-	while (n < 98)
+	for (; n < 98; n++)
 	{
 		printf("%d", n);
-
 		if (n == 98)
-		{
 			printf("\n");
-		}
 		else
-		{
 			printf(", ");
-		}
-		n++;
 	}
-
-	while (n > 98)
-
-	{
+	for (; n > 98; n--)
+    {
 		printf("%d", n);
-
 		if (n == 98)
-		{
 			printf("\n");
-		}
 		else
-		{
 			printf(", ");
-		}
-		n--;
 	}
-
 	if (n == 98)
-	{
 		printf("%d\n", n);
-	}
 }
