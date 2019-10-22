fdb72b7e194bbf19e08119061129190e9bf3d0e7
cbayonao <drcamilob@live.com>
2019-10-22 04:36:04 +0000

    Answer fo the question 1-init_dog.c [1]

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,12 +1,13 @@
 #include "dog.h"
 #include <stdio.h>
-/*
- * init_dog - checkeador
- * @d: dog.
- * @name: name.
- * @age: age.
- * @owner:owner.
- * Return: Always.
+/**
+ * init_dog - check the code for Holberton School students.
+ *@d: d
+ *@name: name
+ *@age: age
+ *@owner: owner
+ *
+ * Return: Always 0.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
