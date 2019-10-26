e64fb2e7b77fd93884719e42400fb6b43d3a142f
cbayonao <drcamilob@live.com>
2019-10-26 13:15:37 -0500

    Fixing 4

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,10 +3,16 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+ * struct select - Structure for operators
+ * @c: name
+ * @f: pointer to function
+ */
 typedef struct select
 {
 	char c;
