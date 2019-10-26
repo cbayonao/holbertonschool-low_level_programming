14b2f5cb2a4c544806e6089635231c704e2a1dbf
cbayonao <drcamilob@live.com>
2019-10-26 13:02:23 -0500

    Second Fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,12 +9,10 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int count;
 	int sum = 0;
+	va_list ap;
 
 	if (n == 0)
 		return (0);
-
-	va_list ap;
-
 	va_start(ap, n);
 	for (count = 0; count < n; count++)
 		sum += va_arg(ap, unsigned int);
