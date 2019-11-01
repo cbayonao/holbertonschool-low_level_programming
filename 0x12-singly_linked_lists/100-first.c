c07b6e778cb56c8becd243f577d7a25c0e7d8992
cbayonao <drcamilob@live.com>
2019-10-31 20:49:45 -0500

    [Answer for 100 advanced]

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+void bull(void) __attribute__ ((constructor));
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void bull(void)
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+    return;
+}
