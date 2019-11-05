ca466487e2273877b5f78c9ffca9a24c3ad7cd51
cbayonao <drcamilob@live.com>
2019-11-05 09:07:09 -0500

    [Attemp 2 for 3 question]

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,8 +18,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = newnode;
 		return (NULL);
 	}
-	while (last->next != NULL)
-		last = last->next;
+	else
+	{
+		while (last->next != NULL)
+			last = last->next;
+	}
 	last->next = newnode;
 	return (newnode);
 }
