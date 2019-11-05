08884c5719662f34b1115df5b6748b04a75242ca
cbayonao <drcamilob@live.com>
2019-11-05 14:39:56 -0500

    [Attemp 1 for question 7]

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * pop_listint - print singly linked list.
+ * @head: pointer to list.
+ *
+ * Return: int.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int x;
+
+	x = 0;
+	while (x != index)
+	{
+		if (head == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			head = head->next;
+			x++;
+		}
+	}
+	return (head);
+}
