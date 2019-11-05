7e9986175665db63169a4c5a969c21fa87d035d2
cbayonao <drcamilob@live.com>
2019-11-04 19:16:19 -0500

    Answer for 4-free_list.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * free_list - Check
+ *@head: unsigned int
+ *Return: x
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+	if (head->next != NULL)
+		free_listint(head->next);
+	free(head);
+}
