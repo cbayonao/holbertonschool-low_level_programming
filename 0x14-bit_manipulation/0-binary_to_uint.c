d4652cc2006390aa644426de03a1843b95396b9d
cbayonao <drcamilob@live.com>
2019-11-07 17:48:30 -0500

    Aswering the question 0, attemp [1]

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,24 @@
+#include "holberton.h"
+/**
+ * sum_listint - print singly linked list.
+ * @head: pointer to list.
+ *
+ * Return: int.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int val = 0;
+	int i = 0;
 
+	if (b == NULL)
+		return (0);
+	while (b[i] != '\0')
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		val <<= 1;
+		val += b[i] - '0';
+		i++;
+	}
+	return (val);
+}
