fc0dbc47ffec1916f5c8991db19a232c5f4c6cc4
cbayonao <drcamilob@live.com>
2019-11-07 09:55:32 -0500

    adding file

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * sum_listint - print singly linked list.
+ * @head: pointer to list.
+ *
+ * Return: int.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
+}
