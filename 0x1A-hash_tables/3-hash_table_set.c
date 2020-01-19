f656e1f82cf8b60144e170cb1365539e5a46b3b1
cbayonao <drcamilob@live.com>
2020-01-19 14:16:49 -0500

    3.

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - 
+ * @ht: Is the hash table to add or update the key/value
+ * @key: Is the key. key can not be an empty string
+ * @value: Is the value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+}
