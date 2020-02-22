05771ea656824b660b53feae53f44c413c3d97d3
cbayonao <drcamilob@live.com>
2020-02-22 02:43:07 +0000

    [Fix] Shebang for question 5.3

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
 """
-Find the number of covered side for grid
+Function to calculate the area of island given a grid
 """
+
+
 def numofneighbour(grid, i, j):
-    count = 0
+	"""Find the number of covered side for grid"""
 
-    if (i > 0 and grid[i - 1][j]): 
-        count += 1
-    if (j > 0 and grid[i][j - 1]):
-        count += 1
-    if (i < len(grid[i]) -1 and grid[i + 1][j]):
-        count += 1
-    if (j < len(grid) and grid[i][j + 1]):
-        count += 1
-    return count
+	count = 0
 
-"""
-Returns the perimeter of the island described in grid
-"""
+	if (i > 0 and grid[i - 1][j]):
+		count += 1
+	if (j > 0 and grid[i][j - 1]):
+		count += 1
+	if (i < len(grid[i]) -1 and grid[i + 1][j]):
+		count += 1
+	if (j < len(grid) and grid[i][j + 1]):
+		count += 1
+
+	return count
+
 def island_perimeter(grid):
-    perimeter = 0
+	"""Returns the perimeter of the island described in grid"""
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if (grid[i][j]):
-                perimeter += (4 - numofneighbour(grid, i, j))
-    return perimeter
+	perimeter = 0
+
+	for i in range(len(grid)):
+		for j in range(len(grid[i])):
+			if (grid[i][j]):
+				perimeter += (4 - numofneighbour(grid, i ,j))
+	return perimeter
